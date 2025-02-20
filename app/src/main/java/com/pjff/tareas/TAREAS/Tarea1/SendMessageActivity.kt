d3e8e36bef37b87750f8ec6b47233e63c6e82655
parisollie <pjff18@gmail.com>
2025-02-20 01:03:46 -0600

    Listo

--- app/src/main/java/com/pjff/tareas/TAREAS/Tarea1/SendMessageActivity.kt
+++ app/src/main/java/com/pjff/tareas/TAREAS/Tarea1/SendMessageActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.pjff.tareas.R
-
+//Ya fue actualizada.
 class SendMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
